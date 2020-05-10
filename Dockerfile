dcb7aa6350e11e1784368ed20256a87a3c98bcac
Eytan Avisror <eytan.avisror@hotmail.com>
2020-05-10 12:35:03 -0700

    Add omitempty for groups & build/SDK updates (#9)
    * allow groups to omitempty
    
    * remove vendor
    
    * fix build to show version
    
    * Add entrypoint and change default command
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * update client-go to 1.16.9
    
    * update go version
    
    * Update .travis.yml

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,21 @@
-FROM golang:1.12-alpine AS builder
+FROM golang:1.13-alpine
 
 RUN apk add --update \
     python \
     python-dev \
     py-pip \
     build-base \
+    git \
   && pip install awscli
 
 WORKDIR /go/src/github.com/keikoproj/aws-auth
 COPY . .
+RUN git rev-parse HEAD
+RUN date +%FT%T%z
 RUN make build
 RUN cp ./bin/aws-auth /bin/aws-auth \
     && chmod +x /bin/aws-auth
 ENV HOME /root
 
-CMD ["/bin/aws-auth"]
+ENTRYPOINT ["/bin/aws-auth"]
+CMD ["help"]
--- cmd/cli/version.go
+++ cmd/cli/version.go
@@ -29,26 +29,30 @@ var (
 	// buildDate in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
 	//It should be set during build via -ldflags.
 	buildDate string
+	// version is the aws-auth package version
+	pkgVersion string = "0.1.2"
 )
 
 // Info holds the information related to descheduler app version.
 type Info struct {
-	GitCommit string `json:"gitCommit"`
-	BuildDate string `json:"buildDate"`
-	GoVersion string `json:"goVersion"`
-	Compiler  string `json:"compiler"`
-	Platform  string `json:"platform"`
+	PackageVersion string `json:"pkgVersion"`
+	GitCommit      string `json:"gitCommit"`
+	BuildDate      string `json:"buildDate"`
+	GoVersion      string `json:"goVersion"`
+	Compiler       string `json:"compiler"`
+	Platform       string `json:"platform"`
 }
 
 // Get returns the overall codebase version. It's for detecting
 // what code a binary was built from.
 func Get() Info {
 	return Info{
-		GitCommit: gitCommit,
-		BuildDate: buildDate,
-		GoVersion: runtime.Version(),
-		Compiler:  runtime.Compiler,
-		Platform:  fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+		GitCommit:      gitCommit,
+		BuildDate:      buildDate,
+		GoVersion:      runtime.Version(),
+		Compiler:       runtime.Compiler,
+		Platform:       fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+		PackageVersion: pkgVersion,
 	}
 }
 
